a60bfd981b59204c8c7df7755239113f6bd3acbc
Artem Abramyan <artemabramyan7@icloud.com>
2020-07-25 11:51:37 +0300

    Extend Bird class

--- lib/data model/bird.dart
+++ lib/data model/bird.dart
@@ -2,15 +2,18 @@ class Bird {
   String name;
   String description;
   String imageURL;
+  String movement;
 
   Bird(
     this.name,
     this.description,
     this.imageURL,
+    this.movement,
   );
 
   Bird.fromJson(Map<String, dynamic> json)
       : name = json["name"],
         description = json["description"],
-        imageURL = json["imageURL"];
+        imageURL = json["imageURL"],
+        movement = json["movement"];
 }
